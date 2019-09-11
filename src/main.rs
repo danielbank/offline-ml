820797f1085b9fa83d130759dd5f168cc9b472a9
Daniel Bank <daniel.p.bank@gmail.com>
2019-09-11 14:59:03 -0700

    predicition is coming back

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use gotham::router::builder::{build_simple_router, DefineSingleRoute, DrawRoutes
 use gotham::router::Router;
 use gotham::state::{FromState, State};
 
-// use tract_core::ndarray;
+use tract_core::ndarray;
 use tract_core::prelude::*;
 
 // fn init_plan() -> SimplePlan<TensorInfo, Borrow<ModelImpl<TI, O>>> {
@@ -33,27 +33,42 @@ use tract_core::prelude::*;
 //   plan;
 // }
 
-// fn prediction_handler(state: State) -> (State, &'static str) {
-//   // open image, resize it and make a Tensor out of it
-//   let image = image::open("grace_hopper.jpg").unwrap().to_rgb();
-//   let resized = image::imageops::resize(&image, 224, 224, ::image::FilterType::Triangle);
-//   let image: Tensor = ndarray::Array4::from_shape_fn((1, 224, 224, 3), |(_, y, x, c)| {
-//       resized[(x as _, y as _)][c] as f32 / 255.0
-//   })
-//   .into();
+fn prediction_handler(state: State) -> (State, &'static str) {
+  // load the model
+  let mut model =
+      tract_tensorflow::tensorflow().model_for_path("mobilenet_v2_1.4_224_frozen.pb").unwrap();
 
-//   // run the plan on the input
-//   let result = plan.run(tvec!(image))?;
+  // specify input type and shape
+  model.set_input_fact(0, TensorFact::dt_shape(f32::datum_type(), tvec!(1, 224, 224, 3))).unwrap();
 
-//   // find and display the max value with its index
-//   let best = result[0]
-//       .to_array_view::<f32>()?
-//       .iter()
-//       .cloned()
-//       .zip(1..)
-//       .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-//   (state, best)
-// }
+  // optimize the model and get an execution plan
+  let model = model.into_optimized().unwrap();
+  let plan = SimplePlan::new(&model).unwrap();
+
+  // open image, resize it and make a Tensor out of it
+  let image = image::open("grace_hopper.jpg").unwrap().to_rgb();
+  let resized = image::imageops::resize(&image, 224, 224, ::image::FilterType::Triangle);
+  let image: Tensor = ndarray::Array4::from_shape_fn((1, 224, 224, 3), |(_, y, x, c)| {
+      resized[(x as _, y as _)][c] as f32 / 255.0
+  })
+  .into();
+
+  // run the plan on the input
+  let result = plan.run(tvec!(image)).unwrap();
+
+  // find and display the max value with its index
+  let best = result[0]
+      .to_array_view::<f32>().unwrap()
+      .iter()
+      .cloned()
+      .zip(1..)
+      .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+  match best {
+      Some(x) => (state, "Holy fuck we got a prediction"),
+      None    => (state, "FML"),
+  }
+}
 
 /// Extracts the elements of the POST request and responds with the form keys and values
 fn form_handler(mut state: State) -> Box<HandlerFuture> {
@@ -80,27 +95,16 @@ fn form_handler(mut state: State) -> Box<HandlerFuture> {
 }
 
 /// Create a `Router`
-fn router(plan: SimplePlan) -> Router {
+fn router() -> Router {
   build_simple_router(|route| {
-    // route.get("/").to(prediction_handler);
+    route.get("/").to(prediction_handler);
     route.post("/").to(form_handler);
   })
 }
 
 /// Start a server and use a `Router` to dispatch requests
 pub fn main() {
-  // load the model
-  let mut model =
-      tract_tensorflow::tensorflow().model_for_path("mobilenet_v2_1.4_224_frozen.pb").unwrap();
-
-  // specify input type and shape
-  model.set_input_fact(0, TensorFact::dt_shape(f32::datum_type(), tvec!(1, 224, 224, 3))).unwrap();
-
-  // optimize the model and get an execution plan
-  let model = model.into_optimized().unwrap();
-  let plan = SimplePlan::new(&model).unwrap();
-
   let addr = "127.0.0.1:7878";
   println!("Listening for requests at http://{}", addr);
-  gotham::start(addr, router(plan))
+  gotham::start(addr, router())
 }
