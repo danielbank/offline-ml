e4b7e8931828fcb49214cb8f68ee91334b13a23b
Daniel Bank <daniel.p.bank@gmail.com>
2019-10-06 21:28:04 -0700

    Maybe some code that is going towards middleware... maybe...

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ use hyper::{Body, StatusCode};
 
 use gotham::handler::{HandlerFuture, IntoHandlerError};
 use gotham::helpers::http::response::create_response;
-use gotham::router::builder::{build_simple_router, DefineSingleRoute, DrawRoutes};
+use gotham::middleware::state::StateMiddleware;
+use gotham::pipeline::single_middleware;
+use gotham::pipeline::single::single_pipeline;
+use gotham::router::builder::{build_router, DefineSingleRoute, DrawRoutes};
 use gotham::router::Router;
 use gotham::state::{FromState, State};
 use regex::bytes::Regex;
@@ -23,23 +26,6 @@ fn prediction_handler(mut state: State) -> Box<HandlerFuture> {
     .concat2()
     .then(|full_body| match full_body {
       Ok(valid_body) => {
-        // load the model
-        let mut model = tract_tensorflow::tensorflow()
-          .model_for_path("mobilenet_v2_1.4_224_frozen.pb")
-          .unwrap();
-
-        // specify input type and shape
-        model
-          .set_input_fact(
-            0,
-            TensorFact::dt_shape(f32::datum_type(), tvec!(1, 224, 224, 3)),
-          )
-          .unwrap();
-
-        // optimize the model and get an execution plan
-        let model = model.into_optimized().unwrap();
-        let plan = SimplePlan::new(&model).unwrap();
-
         // extract the image from the body as input
         let body_content = valid_body.into_bytes();
         let re = Regex::new(r"\r\n\r\n").unwrap();
@@ -80,7 +66,28 @@ fn prediction_handler(mut state: State) -> Box<HandlerFuture> {
 
 /// Create a `Router`
 fn router() -> Router {
-  build_simple_router(|route| {
+  // load the model
+  let mut model = tract_tensorflow::tensorflow()
+    .model_for_path("mobilenet_v2_1.4_224_frozen.pb")
+    .unwrap();
+
+  // specify input type and shape
+  model
+    .set_input_fact(
+      0,
+      TensorFact::dt_shape(f32::datum_type(), tvec!(1, 224, 224, 3)),
+    )
+    .unwrap();
+
+  // optimize the model and get an execution plan
+  let model = model.into_optimized().unwrap();
+  let plan = SimplePlan::new(&model).unwrap();
+
+  let state_data_middleware = StateMiddleware::new(plan);
+  let pipeline = single_middleware(state_data_middleware);
+  let (chain, pipelines) = single_pipeline(pipeline);
+
+  build_router(chain, pipelines, |route| {
     route.post("/").to(prediction_handler);
   })
 }
